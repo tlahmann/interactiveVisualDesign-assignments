6682105369b1b22dc3a5356078c61d3cf22b8d77
tlahmann <tobiaslahmann@gmail.com>
2020-03-28 15:41:13 +0100

    Add legend and tooltip

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -2,18 +2,24 @@
 const margin = {top: 400, right: 0, bottom: 0, left: 0};
 const chartDiv = document.getElementById('chart');
 
-chartData = data.filter(d => ['DEU'].includes(d['countryCode']));
+chartData = data.filter(d => ['DEU', 'FRA', 'ITA', 'ESP'].includes(d['countryCode']));
 
 d3.select('svg').remove();
 const svg = d3.select(chartDiv).append('svg')
 const xAxis = svg.append('g')
-    .attr('class', 'x axis');
+    .attr('id', 'x-axis');
 const yAxis = svg.append('g')
-    .attr('class', 'y axis');
+    .attr('id', 'y-axis');
 const grid = svg.append('g')
-    .attr('class', 'grid');
+    .attr('id', 'grid');
 const dataRegion = svg.append('g')
     .attr('id', 'data');
+let legend = svg.append('g')
+    .attr('id', 'legend');
+let tooltip = d3.select('body')
+    .append('div')
+    .attr('id', 'tooltip');
+let myColors = ["#0055A4", "#000000", "#008C45", "#DA0914"];
 
 function redraw () {
     svg.attr('viewBox', [0, 0, chartDiv.clientWidth, chartDiv.clientHeight]);
@@ -34,10 +40,29 @@ function redraw () {
     // Define axis x and y functions
     let x = d3.scaleLinear()
         .domain(minMaxYear)
-        .range([50, chartDiv.clientWidth - 50]);
+        .range([50, chartDiv.clientWidth - 200]);
     let y = d3.scaleLog()
         .domain(minMaxValue)
         .range([chartDiv.clientHeight - 50, 50]);
+    let gg = g => g
+        .attr("stroke", "currentColor")
+        .attr("stroke-opacity", 0.15)
+        .call(g => g.append("g")
+            .selectAll("line")
+            .data(x.ticks())
+            .join("line")
+            .attr("x1", d => 0.5 + x(d))
+            .attr("x2", d => 0.5 + x(d))
+            .attr("y1", 50)
+            .attr("y2", chartDiv.clientHeight - 50))
+        .call(g => g.append("g")
+            .selectAll("line")
+            .data(y.ticks())
+            .join("line")
+            .attr("y1", d => 0.5 + y(d))
+            .attr("y2", d => 0.5 + y(d))
+            .attr("x1", 50)
+            .attr("x2", chartDiv.clientWidth - 200));
 
     xAxis.call(g => g
         .attr('transform', `translate(0,${chartDiv.clientHeight - 50})`)
@@ -46,6 +71,11 @@ function redraw () {
         .attr('transform', `translate(50,0)`)
         .call(d3.axisLeft(y)
             .tickFormat((d) => d / 1000000 + 'M')));
+    grid.call(gg);
+
+    let c = d3.scaleOrdinal()
+        .domain(chartData.map(d => d.countryCode))
+        .range(myColors);
 
     dataRegion.selectAll('rect')
         .data(chartData)
@@ -54,15 +84,66 @@ function redraw () {
         .attr('y', d => y(+d.value))
         .attr('width', '0')
         .attr('height', '0')
-        .attr('transform', `translate(0,0)`);
+        .attr('transform', `translate(0,0)`)
+        .attr('fill', d => c(d.countryCode));
     dataRegion.selectAll('rect')
         .transition()
         .delay(function (_, i) {return 5 * i;})
         .duration(270)
         .ease(d3.easeExp)
-        .attr('width', '5')
-        .attr('height', '5')
-        .attr('transform', `translate(-2.5,-2.5)`);
+        .attr('width', '10')
+        .attr('height', '10')
+        .attr('transform', `translate(-5,-5)`);
+
+    // Update Legend
+    let legend = svg.selectAll('.legend')
+        .data([...new Set(chartData.map(item => item.countryName))])
+        .enter()
+        .append('g')
+        .attr('class', d => `legend`)
+        .attr('transform', (d, i) => `translate(${chartDiv.clientWidth - 125},${i * 20 + 9 + 250})`)
+        .on('mouseover', function (d) {
+            dataRegion.selectAll('rect')
+                .data(chartData)
+                .filter(v => v.countryName === d)
+                .attr('fill', d => c(d.countryCode));
+            dataRegion.selectAll('rect')
+                .data(chartData)
+                .filter(v => v.countryName !== d)
+                .attr('fill', d => '#ddd');
+        })
+        .on('mouseout', function () {
+            dataRegion.selectAll('rect')
+                .data(chartData)
+                .attr('fill', d => c(d.countryCode));
+        });
+
+    legend.append('rect')
+        .attr('width', 10)
+        .attr('height', 10)
+        .style('fill', d => c(d));
+    legend.append('text')
+        .attr('x', 20)
+        .attr('y', 10)
+        .text(d => d);
+
+    // Update Tooltip
+    svg.selectAll('rect')
+        .on('mouseover', function (d) {
+            if (!d.value) return null;
+            tooltip.html('<b>' + d.countryName + '</b><br/>' + d.year + '<br/><small>' + ((d.value / 1000000).toFixed(2) + ' M') + '</small>');
+            tooltip.style('display', 'block');
+            tooltip.style('opacity', 1);
+        })
+        .on('mousemove', function (d) {
+            if (!d.value) return null;
+            tooltip.style('top', (d3.event.y + 10) + 'px')
+                .style('left', (d3.event.x - 25) + 'px');
+        })
+        .on('mouseout', function () {
+            tooltip.style('display', 'none');
+            tooltip.style('opacity', 0);
+        });
 }
 
 redraw()
